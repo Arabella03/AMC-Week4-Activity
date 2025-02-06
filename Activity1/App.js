a61a910622763d77c058a144f5fb31b276fd8cab
Arabella03 <151042775+Arabella03@users.noreply.github.com>
2025-02-06 16:51:17 +0800

    ScrollView

---
+++ Activity1/App.js
@@ -0,0 +1,314 @@
+import React from 'react';
+import {StyleSheet, Text, ScrollView, StatusBar} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const App = () => (
+ 
+    
+      <ScrollView >
+        <Text>
+          SCV
+        </Text>
+        <Text >
+          SCV
+        </Text>
+        <Text >
+          SCV
+        </Text>
+        <Text>
+          SCV
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text><Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text><Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text><Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text><Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>v
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text><Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+
+        <Text >
+          SCV
+          
+        </Text>
+        <Text >
+          SCV
+          
+        </Text>
+
+
+
+
+
+
+
+
+        
+      
+       
+
+      </ScrollView>
+   
+);
+
+export default App;
